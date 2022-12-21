09aa81642befae4eb8583a12e93939968e6d0f6f
Gillian Assi <53274850+gillianassi@users.noreply.github.com>
2022-12-21 18:20:34 +0100

    Added implementation using Oculus provided components
    Shape recogniser and transform recogniser

---
+++ Assets/Game/Scripts/Environment/PoseNameVisualiser.cs
@@ -0,0 +1,27 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Oculus.Interaction;
+
+public class PoseNameVisualiser : MonoBehaviour
+{
+    public List<ActiveStateSelector> Poses;
+    public TMPro.TextMeshPro Text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(var pose in Poses)
+        {
+            pose.WhenSelected += () => SetTextToPoseName(pose.gameObject.name);
+            pose.WhenUnselected += () => SetTextToPoseName("");
+        }
+    }
+
+    // Update is called once per frame
+    void SetTextToPoseName(string newText)
+    {
+        Text.text = newText;
+    }
+}
